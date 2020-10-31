3e098d7f2f5b689577ae33d539b29072f7881a79
ukmook <mchurn.cvml@googlemail.com>
2020-10-31 18:23:36 +0000

    UAVCAN: add f413 to can build

--- libraries/AP_HAL_ChibiOS/hwdef/common/flash.c
+++ libraries/AP_HAL_ChibiOS/hwdef/common/flash.c
@@ -44,7 +44,7 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Modified for use in AP_HAL by Andrew Tridgell and Siddharth Bharat Purohit
  */
 
@@ -88,6 +88,12 @@ static const uint32_t flash_memmap[STM32_FLASH_NPAGES] = { KB(16), KB(16), KB(16
 #define STM32_FLASH_NPAGES  12
 static const uint32_t flash_memmap[STM32_FLASH_NPAGES] = { KB(16), KB(16), KB(16), KB(16), KB(64),
                                                            KB(128), KB(128), KB(128), KB(128), KB(128), KB(128), KB(128) };
+
+#elif BOARD_FLASH_SIZE == 1536
+#define STM32_FLASH_NPAGES  16
+static const uint32_t flash_memmap[STM32_FLASH_NPAGES] = { KB(16), KB(16), KB(16), KB(16), KB(64),
+                                                           KB(128), KB(128), KB(128), KB(128), KB(128), KB(128), KB(128),
+                                                           KB(128), KB(128), KB(128), KB(128)};
 
 #elif BOARD_FLASH_SIZE == 2048
 #define STM32_FLASH_NPAGES  24
@@ -492,7 +498,7 @@ static bool stm32_flash_write_f4f7(uint32_t addr, const void *buf, uint32_t coun
 #if STM32_FLASH_DISABLE_ISR
     syssts_t sts = chSysGetStatusAndLockX();
 #endif
-    
+
     stm32_flash_unlock();
 
     // clear previous errors
@@ -510,7 +516,7 @@ static bool stm32_flash_write_f4f7(uint32_t addr, const void *buf, uint32_t coun
 
         // ensure write ordering with cache
         __DSB();
-        
+
         stm32_flash_wait_idle();
 
         const uint32_t v2 = getreg32(addr);
@@ -533,7 +539,7 @@ static bool stm32_flash_write_f4f7(uint32_t addr, const void *buf, uint32_t coun
 
         // ensure write ordering with cache
         __DSB();
-        
+
         stm32_flash_wait_idle();
 
         if (getreg16(addr) != *(uint16_t *)b) {
@@ -588,7 +594,7 @@ static bool stm32_flash_write_f1(uint32_t addr, const void *buf, uint32_t count)
 #if STM32_FLASH_DISABLE_ISR
     syssts_t sts = chSysGetStatusAndLockX();
 #endif
-    
+
     stm32_flash_unlock();
 
     stm32_flash_wait_idle();
@@ -651,9 +657,8 @@ void stm32_flash_keep_unlocked(bool set)
         flash_keep_unlocked = true;
     } else if (!set && flash_keep_unlocked) {
         flash_keep_unlocked = false;
-        stm32_flash_lock();        
+        stm32_flash_lock();
     }
 }
 
 #endif // HAL_NO_FLASH_SUPPORT
-
